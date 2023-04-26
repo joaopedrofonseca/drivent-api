f16c8bafea71ad8e4f930d62ae9ebd1bfeea110f
joaopedrofonseca <joaopedrofonsec4@gmail.com>
2023-04-25 22:03:43 -0300

    feat: creating booking router

---
+++ src/controllers/booking-controller.ts
@@ -0,0 +1,11 @@
+export async function getBooking() {
+
+}
+
+export async function createBooking() {
+
+}
+
+export async function updateBooking() {
+
+}
---
+++ src/routers/booking-router.ts
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+import { authenticateToken } from '@/middlewares';
+import { createBooking, getBooking, updateBooking } from '@/controllers/booking-controller';
+
+const bookingRouter = Router();
+
+bookingRouter
+    .all('/*', authenticateToken)
+    .get('/', getBooking)
+    .post('/', createBooking)
+    .put('/:bookingId', updateBooking);
+
+export default bookingRouter;
